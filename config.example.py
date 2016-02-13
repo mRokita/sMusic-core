2ba5d8118799293ab2e08f5622542b39ac6cbf98
Artur Puzio <cytadela88@gmail.com>
2016-02-13 20:30:36 +0100

    Dodano przykladowa konfiguracje dzownkow do config.example.py

--- config.example.py
+++ config.example.py
@@ -2,3 +2,6 @@ ssl_validate_cert = False
 server_port = 3484
 server_host = "localhost"
 server_key = "dafekIWska"
+breaks = [["7:45", "8:15" ],
+          ["9:00", "9:10" ],
+          ["9:55", "10:10"]]
