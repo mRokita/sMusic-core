8f9a73708e347c6d89417d17ce1c6badd2b95a13
mRokita <mrokita@mrokita.pl>
2016-03-11 20:34:24 +0100

    Przenoszę logowanie do oddzielnego modułu

---
+++ smusicclient/logs.py
@@ -0,0 +1,28 @@
+import logging
+import os
+import config
+import datetime
+
+
+def setup_logging():
+    if not os.path.exists(config.log_path):
+        os.makedirs(config.log_path)
+    log_file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler("{0}/{1}.log".format(config.log_path, log_file_name))
+    file_handler.setFormatter(log_formatter)
+    root_logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_formatter)
+    root_logger.addHandler(console_handler)
+
+
+print_info = logging.info
+print_warning = logging.warning
+print_debug = logging.debug
+print_error = logging.error
